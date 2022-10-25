02269225ef1e88a59c5ff70c7ecdb425eec82a16
LiviuDragomirescu <dragomiresculiviu@gmail.com>
2022-10-25 12:25:39 +0300

    Template

---
+++ App/pages/Intro.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+export default Intro = ({img, button_name}) => {
+    return (
+    <View>
+      <Text>
+        {button_name}
+      </Text>
+    </View>
+    )
+}
