c09141b0d7457d2720596773ef029142b7715b89
cadaralex12 <alexcadar2000@gmail.com>
2022-11-08 16:38:43 +0200

    created ProgressHistory page

---
+++ App/pages/ProgressHistory.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+const image = require('.././images/Arnold.png') ;
+
+const Progress_History = ({navigation}) => {
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+
+  export default Progress_History;
