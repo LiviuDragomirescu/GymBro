d3b091e91a7b1ad7090baf3108a882424e468649
cadaralex12 <alexcadar2000@gmail.com>
2022-11-15 17:11:24 +0200

    added Login page

---
+++ App/pages/Login.js
@@ -0,0 +1,15 @@
+import { View } from 'react-native';
+
+import Template_Button from '../components/Template_Button';
+
+
+
+const Login = ({navigation}) => {
+    return (
+      <View>
+        <Template_Button navigation={navigation} ></Template_Button>
+      </View>
+    )
+  }
+
+  export default Login;
